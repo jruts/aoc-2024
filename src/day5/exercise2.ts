9c759dc308b45b071c72b5e58a5a0668f1e8cc3f
jruts <jim.ruts+work@gmail.com>
2024-12-05 19:18:55 +0100

    day5_exercise2

--- src/day5/exercise2.ts
+++ src/day5/exercise2.ts
@@ -2,32 +2,17 @@ import { rules, sumMiddlePageNumbers, updates } from "./exercise1.ts";
 
 function getIntersection(a: number[], b: number[]) {
 	const s = new Set<number>(b);
-	return [...new Set<number>(a)].filter((x) => s.has(x));
+	return [...new Set<number>(a)].filter((x) => s.has(x))?.[0];
 }
 
-// const invalidUpdates: number[][] = [];
+function fixInvalidUpdate(
+	update: number[],
+	valid: boolean = false,
+	loop: number = 0,
+) {
+	if (valid && loop === 1) return [];
+	if (valid) return update;
 
-// for (const update of updates) {
-// 	let isValid = false;
-// 	const checkedPages: number[] = [];
-// 	for (const page of update) {
-// 		const intersection = getIntersection(
-// 			checkedPages,
-// 			rules.get(page) || [],
-// 		);
-// 		if (intersection.length) {
-// 			console.log(intersection);
-// 			isValid = true;
-// 			break;
-// 		}
-// 		checkedPages.push(page);
-// 	}
-// 	if (isValid) {
-// 		invalidUpdates.push(update);
-// 	}
-// }
-
-function fixInvalidUpdates(update: number[], loop: number = 0) {
 	const checkedPages: number[] = [];
 
 	let isValid = true;
@@ -36,27 +21,21 @@ function fixInvalidUpdates(update: number[], loop: number = 0) {
 			checkedPages,
 			rules.get(page) || [],
 		);
-		if (intersection.length) {
+		checkedPages.push(page);
+		if (intersection) {
 			const currentIdx = update.indexOf(page);
-			const interIdx = update.indexOf(intersection[0]);
-			update[currentIdx] = intersection[0];
+			const interIdx = update.indexOf(intersection);
+			update[currentIdx] = intersection;
 			update[interIdx] = page;
 			isValid = false;
 			break;
 		}
-		checkedPages.push(page);
 	}
 
-	if (isValid && loop === 0) return [];
-	if (!isValid) {
-		return fixInvalidUpdates(update, ++loop);
-	} else {
-		return update;
-	}
+	return fixInvalidUpdate(update, isValid, ++loop);
 }
 
-const fixedInvalidUpdates = updates.map((update) =>
-	fixInvalidUpdates(update, 0)
-).filter((x) => x.length);
+const fixedInvalidUpdates = updates.map((update) => fixInvalidUpdate(update))
+	.filter((x) => x.length);
 
 console.log(sumMiddlePageNumbers(fixedInvalidUpdates));
